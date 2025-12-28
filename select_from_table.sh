a08c7bc3231c46a0393af94af146060bfaaa4ddd
alaa atef <alaaatef@localhost.localdomain>
2025-12-28 11:49:11 +0200

    select_code

---
+++ select_from_table.sh
@@ -0,0 +1,106 @@
+db=$1
+echo "enter the table name :"
+read tname
+if [ -f "$db/$tname.data" ]
+then
+	echo "enter the column names (comma-separated) :"
+	read cols
+	if [ "$cols" = "*" ]
+	then
+		select_all=true
+	else
+		select_all=false
+		IFS=',' read -ra col_arr <<< "$cols"
+		        fields_to_print=()
+        for c in "${col_arr[@]}"
+        do
+            if ! grep -q "^$c:" "$db/$tname.meta"; then
+                echo "column $c doesn't exist"
+                exit
+            fi
+            counter=1
+            while read line; do
+                colname=$(echo "$line" | cut -d: -f1)
+                if [ "$colname" = "$c" ]; then
+                    fields_to_print+=("$counter")
+                    break
+                fi
+                counter=$((counter + 1))
+            done < "$db/$tname.meta"
+        done
+    fi
+	echo "do you want to filter rows (condition) ?"
+	read ans
+	if [ "$ans" = "yes" ]
+	then
+		echo "write the condition(column operator value) , operator should be (> , < , =) for int , (=) for string"
+		read  col op val
+		counter=1
+		field_number=""
+        while read line; do
+            colname=$(echo "$line" | cut -d: -f1)
+            datatype=$(echo "$line" | cut -d: -f2)
+            if [ "$col" = "$colname" ]; then
+                field_number=$counter
+                col_datatype="$datatype"
+                break
+            fi
+            counter=$((counter + 1))
+        done < "$db/$tname.meta"
+
+	if [ -z "$field_number" ]; then
+            echo "column '$col' not found in meta file"
+            exit 1
+        fi
+	fi
+
+else
+	echo "table $tname doesn't exist"
+	exit
+fi
+found=false
+
+while read line
+do
+	match=true
+	if [ "$ans" = "yes" ]
+	then
+        	value=$(echo "$line" | cut -d~ -f$field_number)
+        	match=false
+	if [ "$col_datatype" = "int" ]; then
+		  if ! [[ "$value" =~ ^-?[0-9]+$ && "$val" =~ ^-?[0-9]+$ ]]; then
+                continue
+            fi
+        if [ "$op" = "=" ] && [ "$value" -eq "$val" ]; then
+           match=true
+        elif [ "$op" = ">" ] && [ "$value" -gt "$val" ]; then
+           match=true
+        elif [ "$op" = "<" ] && [ "$value" -lt "$val" ]; then
+            match=true
+        fi
+    else 
+        if [ "$op" = "=" ] && [ "$value" = "$val" ]; then
+            match=true
+        fi
+    fi
+	fi
+  
+     if [ "$match" = true ]; then
+	      found=true
+        if [ "$select_all" = true ]; then
+            echo "$line"
+        else
+            output=""
+            for f in "${fields_to_print[@]}"; do
+                field_val=$(echo "$line" | cut -d~ -f$f)
+                output+="$field_val "
+            done
+            echo "${output}" 
+        fi
+    fi
+	 
+done < "$db/$tname.data"
+if [ "$ans" = "yes" ] && [ "$found" = false ]; then
+    echo "no rows with this value"
+fi
+
